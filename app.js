e3d064d3935b1afdedd0269a56bb2d18e8383d46
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-09-21 12:54:59 -0500

    basic node server init

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('expres');
+var connect = require('connect');
+var port = process.env.PORT || 4242;
+var router = express.Router();
+var app = express();
+
+app.set('port', port);
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+router.use(function (err, req, res, next) {
+  console.log('%s %s %s', req.method, req.url, req.path);
+  next();
+});
+
+router.get('/', function (req, res) {
+  res.resnder ('pages/index');
+});
+
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('Express Running on: ' + port + ' NODE _ENV: ' + process.env.NODE_ENV);
+});
