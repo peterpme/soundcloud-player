1f00b988448fde93119f64d74be254df3388c917
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-09-21 16:26:27 -0500

    changed to scplayer

--- app/js/directives/scPlayer.js
+++ app/js/directives/scPlayer.js
@@ -1,6 +1,17 @@
 soundcloudPlayer.ng.directive('scPlayer', ['$log', function ($log) {
 	var clientId = '6aeca16577b6acfd3e76ab35ac241d81';
 
+	(function init() {
+		SC.initialize({
+			client_id: clientID;
+		})
+		.get('/tracks', {
+			q: 'deep house'
+		}, function (tracks) {
+			$log.info('Tracks:', tracks);
+		});
+	})();
+
 return {
 	restrict: 'A',
 	link: function (scope, element) {
