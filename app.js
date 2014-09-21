350b142fafdacd8ac0bd81acbd531ffa3aed1fd4
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-09-21 13:34:33 -0500

    renamed public to app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 app.set('port', port);
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 
 router.use(function (err, req, res, next) {
   console.log('%s %s %s', req.method, req.url, req.path);
