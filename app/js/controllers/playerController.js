9439c6bcc09ec4151e76370a35fd42837ca7b8f6
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-09-21 18:45:07 -0500

    updated player controller with form submission functions

--- app/js/controllers/playerController.js
+++ app/js/controllers/playerController.js
@@ -1,20 +1,47 @@
 soundcloudPlayer.ng.controller('playerController', ['$scope', '$http', '$log', function ($scope, $http, $log) {
-
-	$log.info('[player-controller] init');
+	$log.info('[playerController] Controller Loaded');
 
 	$scope.clientId = '6aeca16577b6acfd3e76ab35ac241d81';
-	$scope.searchTerm = 'Deep House';
+	$scope.url = 'https://api.soundcloud.com/tracks.json?q=';
 	$scope.results = [];
-	$scope.init = function () {
-		$log.info('init');
+
+	$scope.formSubmitted = false;
+	$scope.formSuccess = false;
+
+	$scope.init = (function () {
+		$log.info('[player-controller] initialized');
 
 		SC.initialize({
 			client_id: $scope.clientId
 		});
 
-		$http.jsonp('https://api.soundcloud.com/tracks.json?q=DEEP_HOUSE&client_id=' + $scope.clientId + '&callback=JSON_CALLBACK')
+	})();
+
+	$scope.submitForm = function () {
+		$log.info('called');
+		$scope.formSubmitted = true;
+		$scope.encodedString = encodeURI($scope.category);
+
+		$scope.searchSoundcloud();
+	};
+
+	$scope.lockForm = function () {
+		$scope.disableForm = true;
+		angular.element('input, button').attr('disabled', 'disabled');
+	}
+
+	$scope.unlockForm = function () {
+		$scope.disableForm = false;
+		angular.element('input, button').removeAttr('disabled');
+	}
+
+	$scope.searchSoundcloud = function () {
+
+		$http.jsonp($scope.url + $scope.encodedString + '&client_id=' + $scope.clientId + '&callback=JSON_CALLBACK')
 		.success( function (data) {
-			$log.info(data, data.genre, data.title);
+			$scope.formSuccess = true;
+
+			$log.debug(data);
 
 			data.forEach(function (i) {
 				$scope.results.push({
@@ -22,12 +49,9 @@ soundcloudPlayer.ng.controller('playerController', ['$scope', '$http', '$log', f
 					genre: i.genre
 				})
 			});
-
-			$log.info('results', $scope.results);
-
 		});
-	}
 
-	$scope.init();
+		return $scope.results;
+	};
 
 }]);
