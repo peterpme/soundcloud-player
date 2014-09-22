2225254c0eb40816b28cd244649fb52da386d7fb
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2014-09-21 20:58:11 -0500

    add track to player logic

--- app/js/controllers/playerController.js
+++ app/js/controllers/playerController.js
@@ -2,7 +2,7 @@ soundcloudPlayer.ng.controller('playerController', ['$scope', '$http', '$log', f
 	$log.info('[playerController] Controller Loaded');
 
 	$scope.clientId = '6aeca16577b6acfd3e76ab35ac241d81';
-	$scope.url = 'https://api.soundcloud.com/tracks.json?q=';
+	$scope.url = 'http://api.soundcloud.com/tracks.json?q=';
 	$scope.trackList = [];
 	$scope.data = {};
 
@@ -16,13 +16,16 @@ soundcloudPlayer.ng.controller('playerController', ['$scope', '$http', '$log', f
 			client_id: $scope.clientId
 		});
 
+		SC.stream("/tracks/293", function(sound){
+			sound.play();
+		});
+
 	})();
 
 	$scope.submitForm = function () {
 		$scope.formSubmitted = true;
 		$scope.encodedString = encodeURI($scope.category);
-
-		$scope.searchSoundcloud();
+		$scope.querySoundcloud();
 	};
 
 	$scope.lockForm = function () {
@@ -35,23 +38,33 @@ soundcloudPlayer.ng.controller('playerController', ['$scope', '$http', '$log', f
 		angular.element('input, button').removeAttr('disabled');
 	}
 
-	$scope.searchSoundcloud = function () {
+	$scope.querySoundcloud = function () {
 
 		$http.jsonp($scope.url + $scope.encodedString + '&client_id=' + $scope.clientId + '&callback=JSON_CALLBACK')
-		.success( function (data) {
+		.success(function (data) {
+
 			$scope.formSuccess = true;
 			$scope.data = data;
 
 			$scope.data.forEach (function (track) {
-
 				$scope.trackList.push({
-					title: track.title
+					title: track.title,
+					uri: track.uri + '/stream?client_id=' + $scope.clientId
 				})
 			});
 
-			$log.debug($scope.trackList);
-
+			$log.info($scope.trackList);
 		});
+
+		$scope.addTrackToPlayer();
+	};
+
+	$scope.addTrackToPlayer = function () {
+		var audio = $scope.trackList;
+		console.log(audio);
+		var audioBox = document.getElementsByTagName('audio');
+
+		audioBox[0].setAttribute('src', audio);
 	};
 
 }]);
